e94d4f68207316bd2b6f74a242259da8a499e875
Abi Raskin <abiraskin@gmail.com>
2018-10-25 02:32:54 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
-def countdown = 10
-  10.times do
-    countdown -= 1
-    puts "#{countdown} SECOND(S)!"
+def countdown
+  number = 10
+  while number > 0
+  countdown -= 1
+  puts "#{countdown} SECOND(S)!"
   end
-end
 puts "HAPPY NEW YEAR!"
 end
