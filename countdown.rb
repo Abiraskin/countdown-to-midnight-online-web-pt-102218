486de8084ae3af3209a61de676605b5024bcd927
Abi Raskin <abiraskin@gmail.com>
2018-10-25 02:49:08 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,16 @@
-def countdown(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    seconds_to_midnight -= 1
+def countdown(time)
+  while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
   end
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
+def countdown_with_sleep(time)
+  while time > 0
+    puts "#{time} SECOND(S)!"
     sleep(1)
-    seconds_to_midnight -= 1
+    time -= 1
   end
   "HAPPY NEW YEAR!"
 end
